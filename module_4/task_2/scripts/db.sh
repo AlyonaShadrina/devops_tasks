247a94cffd408c19db0383d21663ef298932e8cd
Shadrina Alyona <alyon.shadrina@gmail.com>
2023-03-06 21:03:19 +0200

    module 4: create script for DB managment

---
+++ module_4/task_2/scripts/db.sh
@@ -0,0 +1,57 @@
+# TODO: add full description
+declare -A COMMANDS_HELP
+COMMANDS_HELP[add]="add help"
+COMMANDS_HELP[backup]="backup help"
+COMMANDS_HELP[find]="find help"
+COMMANDS_HELP[list]="list help"
+
+typeset fileName=users.db
+typeset fileDir=../data
+typeset filePath=$fileDir/$fileName
+
+check_table () {
+  if [[ ! -f $filePath ]];
+  then
+      read -r -p "users.db does not exist. Do you want to create it? [Y/n] " answer
+      answer=${answer,,}
+      if [[ "$answer" =~ ^(yes|y)$ ]];
+      then
+          touch $filePath
+          echo "File ${fileName} is created."
+      else
+          echo "File ${fileName} must be created to continue. Try again." >&2
+          exit 1
+      fi
+  fi
+}
+add () {
+  echo "add called"
+  # TODO: Add validation
+  read -p "Enter user name: " username
+  read -p "Enter user role: " role
+
+  echo "${username}, ${role}" | tee -a $filePath
+}
+backup () {
+  echo "backup called"
+}
+find () {
+  echo "find called"
+}
+list () {
+  echo "list called"
+}
+help () {
+  echo "Available commands:"
+  for K in "${!COMMANDS_HELP[@]}"; do
+    # TODO: print as table
+    echo $K ${COMMANDS_HELP[$K]};
+  done
+}
+
+if [[ $(type -t "$1") == function ]]; then
+  check_table
+  echo "$($1)"
+else 
+  help
+fi
