8adbf4c2a6c5154e285343617d19311f86fc6f97
Shadrina Alyona <alyon.shadrina@gmail.com>
2023-03-12 10:28:44 +0200

    lab 1: create script for pipeline update

---
+++ lab_1/scripts/update-pipeline-definition.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+if ! [ -f "$1" ]; then
+  echo "$1 file not found"
+  exit
+fi
+
+packagesNeeded='jq'
+if ! command -v $packagesNeeded &> /dev/null
+then
+  echo "$packagesNeeded command required but could not be found"
+  echo ""
+
+  if [ -x "$(command -v apk)" ];       then echo "sudo apk add --no-cache $packagesNeeded"
+  elif [ -x "$(command -v apt-get)" ]; then echo "sudo apt-get install $packagesNeeded"
+  elif [ -x "$(command -v dnf)" ];     then echo "sudo dnf install $packagesNeeded"
+  elif [ -x "$(command -v zypper)" ];  then echo "sudo zypper install $packagesNeeded"
+  fi
+  
+  exit
+fi
+
+JSON_CONTENT="$(cat $1)"
+
+remove_metadata () {
+  metadata="$(jq '. | .metadata'<<< $JSON_CONTENT)";
+  echo "$metadata"
+
+  if [ $metadata = 'null'  ]; then
+    echo "There is no metadata to remove."
+    exit
+  fi
+
+  JSON_CONTENT="$(jq 'del(.metadata)'<<< $JSON_CONTENT)";
+  echo "Metadata is removed"
+}
+
+remove_metadata
+
+echo "$JSON_CONTENT"
+
+
+
+
+# newFileName="pipeline-$(date +'%Y-%m-%d-%H-%M-%S-%s').json"
+# cp $1 $newFileName
+
+# echo "New pipeline file is created"
+# echo "$newFileName"
