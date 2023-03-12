2be2d4a584995d1e74f9bebc5159a2373a5241d3
Shadrina Alyona <alyon.shadrina@gmail.com>
2023-03-12 16:49:57 +0200

    lab 1: add options for pipeline script

--- lab_1/scripts/update-pipeline-definition.sh
+++ lab_1/scripts/update-pipeline-definition.sh
@@ -2,7 +2,7 @@
 
 if ! [ -f "$1" ]; then
   echo "$1 file not found"
-  exit
+  exit 1
 fi
 
 packagesNeeded='jq'
@@ -17,10 +17,12 @@ then
   elif [ -x "$(command -v zypper)" ];  then echo "sudo zypper install $packagesNeeded"
   fi
   
-  exit
+  exit 1
 fi
 
 JSON_CONTENT="$(cat $1)"
+
+# owner="basename $(dirname $(git remote get-url origin))"
 
 remove_metadata () {
   metadata="$(jq '. | .metadata' <<< $JSON_CONTENT)";
@@ -47,16 +49,81 @@ increment_version () {
   echo "New version is $newNumber"
 }
 
+# OPTS=$(getopt -o '' -a --longoptions 'configuration:,owner:,branch::,poll-for-source-changes::' -n "$0" -- "$@")
+OPTS=$(getopt -o '' -a --longoptions 'configuration:,owner:,branch:,poll-for-source-changes:' -n "$0" -- "$@")
+eval set -- "$OPTS"
+while true; do
+  case "$1" in
+    --configuration )
+        configuration=$2
+        shift 2
+        ;;
+    --owner )
+        owner=$2
+        shift 2
+        ;;
+    --branch )
+        branch=${2}
+        # branch=${2:-"main"}
+        shift 2
+        ;;
+    --poll-for-source-changes )
+        pollForSourceChanges=${2}
+        # pollForSourceChanges=${2:-"false"}
+        shift 2
+        ;;
+    --)
+        shift
+        break
+        ;;
+  esac
+done
+
+# if [ -z "$configuration" ]; then
+#   echo "--configuration was not provided"
+#   exit
+# fi
+
+set_branch () {
+  JSON_CONTENT="$(jq --arg branch $branch '.pipeline.stages[0].actions[0].configuration.Branch = $branch' <<< $JSON_CONTENT)";
+  
+  echo "pipeline.stages[0].actions[0].configuration.Branch : $branch"
+}
+set_owner () {
+  JSON_CONTENT="$(jq --arg owner $owner '.pipeline.stages[0].actions[0].configuration.Owner = $owner' <<< $JSON_CONTENT)";
+  
+  echo "pipeline.stages[0].actions[0].configuration.Owner: $owner"
+}
+set_poll () {
+  JSON_CONTENT="$(jq --arg pollForSourceChanges $pollForSourceChanges '.pipeline.stages[0].actions[0].configuration.PollForSourceChanges = $pollForSourceChanges' <<< $JSON_CONTENT)";
+  
+  echo "pipeline.stages[0].actions[0].configuration.PollForSourceChanges: $pollForSourceChanges"
+}
+set_env () {
+  JSON_CONTENT="${JSON_CONTENT//"{{BUILD_CONFIGURATION value}}"/$configuration}"   
+  
+  echo "{{BUILD_CONFIGURATION value}} replaced with $configuration"
+}
+
 remove_metadata
 increment_version
 
-echo "$JSON_CONTENT"
+if ! [ -z $branch ]; then
+  set_branch
+fi
 
+if ! [ -z "$pollForSourceChanges" ]; then
+  set_poll
+fi
 
+if ! [ -z "$owner" ]; then
+  set_owner
+fi
 
+if ! [ -z "$configuration" ]; then
+  set_env
+fi
 
-# newFileName="pipeline-$(date +'%Y-%m-%d-%H-%M-%S-%s').json"
-# cp $1 $newFileName
+newFileName="pipeline-$(date +'%Y-%m-%d-%H-%M-%S-%s').json"
 
-# echo "New pipeline file is created"
-# echo "$newFileName"
+echo "$JSON_CONTENT" > $newFileName && echo "New pipeline file is created"
